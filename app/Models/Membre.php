b7fe7d8bae10df531f1773d3c35607099cec5173
M Kpagnon <kpagnonb@gmail.com>
2025-03-24 14:58:20 -0400

    Migration 1

--- app/Models/Membre.php
+++ app/Models/Membre.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Membre extends Model
